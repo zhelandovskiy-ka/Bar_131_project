553354457fb61a95636d2141835eba316544075b
sfall <sfall3003@gmail.com>
2023-09-26 16:28:14 +0600

    test

---
+++ src/main/java/units/Config.java
@@ -0,0 +1,11 @@
+package units;
+
+public class Config {
+    public static final String BOT_NAME = "BOT_NAME";
+    public static final String BOT_TOKEN = "BOT_TOKEN";
+    public static final String PAY_URL = "https://yoomoney.ru/"; //payment method 1
+    public static final String PAY_URL_2 = "https://www.tinkoff.ru/"; //payment method 2
+    public static final long BOT_ID_MY = 0; //admin tg id
+    public static final double PERCENT = 0.1; //margin percentage
+    //test
+}
